983922409ea6223e39e84862199dd40212c81e9d
alreadynyeong <kjw87130@naver.com>
2023-07-13 21:55:05 +0900

    Feat: Activity 페이지 생성

---
+++ data/Activity.ts
@@ -0,0 +1,83 @@
+export interface Activity {
+  id: number;
+  title: string;
+  date: string;
+  role: string;
+  content: Content[];
+}
+
+interface Content {
+  id: number;
+  text?: string;
+  project: string;
+  projectNum: number;
+}
+
+export const ActivityList: Activity[] = [
+  {
+    id: 0,
+    title: "GDSC SSWU 4th",
+    date: "2022.09~2023.06",
+    role: "교육팀 팀장",
+    content: [
+      {
+        id: 0,
+        text: "GDSC SSWU 4기 홈페이지 제작",
+        project: "GDSC SSWU",
+        projectNum: 0,
+      },
+      {
+        id: 1,
+        text: "Google Solution Challenge 참가",
+        project: "withParents",
+        projectNum: 1,
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Pycon Korea 2023",
+    date: "2023.01~2023.07",
+    role: "FE 개발 자원봉사자",
+    content: [
+      {
+        id: 0,
+        project: "Pycon Korea 2023",
+        projectNum: 2,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "수정광산 SaltyLab 1기",
+    date: "2022.10~2023.10",
+    role: "Frontend 개발팀",
+    content: [
+      {
+        id: 0,
+        project: "수정광산",
+        projectNum: 3,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "UMC 4th SSWU",
+    date: "2023.03~",
+    role: "Member",
+    content: [
+      {
+        id: 0,
+        text: "4회 NE(O)RDINARY 해커톤 우수상",
+        project: "Re:MEMORY",
+        projectNum: 4,
+      },
+      {
+        id: 1,
+        text: "4회 UMC 해커톤 우수상",
+        project: "Green Quest",
+        projectNum: 5,
+      },
+    ],
+  },
+];
--- pages/activity/index.tsx
+++ pages/activity/index.tsx
@@ -1,8 +1,85 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+import { ActivityList } from "@/data/Activity";
+const Container = styled.div`
+  width: 100%;
+  margin-top: 70px;
+  margin-bottom: 150px;
+`;
+const Box = styled.div`
+  width: 80%;
+  justify-content: center;
+  margin: 0 auto;
+`;
+const Title = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding: 30px;
+  border-bottom: 2px solid black;
+`;
+const Content = styled.div`
+  font-size: 1.2rem;
+  padding: 30px;
+  padding-bottom: 50px;
+  border-bottom: 1px solid lightgray;
+`;
+const Date = styled.span`
+  font-size: 0.8rem;
+  color: gray;
+  margin-left: 10px;
+`;
+const Role = styled.div`
+  font-size: 1rem;
+  margin-top: 30px;
+`;
+const Activitys = styled.div`
+  border-left: 3px solid black;
+`;
+const Actiivty = styled.div`
+  font-size: 1rem;
+  margin-top: 20px;
+  margin-left: 20px;
+`;
+const Project = styled.span`
+  font-size: 0.9rem;
+  margin-right: 7px;
+  color: green;
+`;
+
 const Activity = () => {
   return (
-    <div>
-      <h1>Activity</h1>
-    </div>
+    <Container>
+      <Box>
+        <Title>활동</Title>
+        {ActivityList.slice()
+          .reverse()
+          .map((activity) => (
+            <Content key={activity.id}>
+              [ {activity.title} ] <Date>{activity.date}</Date>
+              <Role>{activity.role}</Role>
+              <Activitys>
+                {activity.content.map((content) => (
+                  <Actiivty key={content.id}>
+                    {content.text}
+                    <div>
+                      <Project>{content.project}</Project>
+                      <a href={`/projects/${content.projectNum}`}>
+                        <Image
+                          src={"/ArrowRight.png"}
+                          width={13}
+                          height={13}
+                          alt={""}
+                        />
+                      </a>
+                    </div>
+                  </Actiivty>
+                ))}
+              </Activitys>
+            </Content>
+          ))}
+      </Box>
+    </Container>
   );
 };
 
