f06db6d6e300e0bcb62dd43691ae8d129f565168
alreadynyeong <kjw87130@naver.com>
2023-12-07 15:25:49 +0900

    Del: 불필요한 코드 제거

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,25 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-import router, { useRouter } from "next/router";
-import { useMediaQuery } from "react-responsive";
 import styled from "styled-components";
 
-import { NavMenu } from "@/data/Route";
-
-import Contact from "../common/contact";
 import MobileMenu from "../common/menu";
 
 const Nav = () => {
-  const [show, setShow] = useState<boolean>(true);
   const [menu, setMenu] = useState<boolean>(false);
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-
-  const path = useRouter().pathname;
-
-  const mobile = useMediaQuery({ query: "(max-width: 768px)" });
-  useEffect(() => {
-    setIsMobile(mobile);
-  }, [mobile]);
 
   return (
     <>
@@ -51,27 +37,6 @@ const Container = styled.div`
   position: fixed;
   box-shadow: 5px 0.2px 5px 1px lightgray;
   overflow: hidden;
-`;
-const MenuContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 30px;
-  padding-left: 30px;
-  margin-right: 10px;
-`;
-const Menu = styled.div`
-  width: fit-content;
-  height: fit-content;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid gray;
-  padding: 10px;
-  text-decoration: none;
-  cursor: pointer;
-  &:hover {
-    color: green;
-  }
 `;
 const Title = styled.div`
   display: flex;
